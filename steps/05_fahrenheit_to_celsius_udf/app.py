17b0026e944ad754c6201584705959a147cc79a9
Adrian Randle <adrian.randle@snowflake.com>
2023-02-19 19:47:05 +0000

    Commit - Test

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -10,15 +10,16 @@
 # SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
 
 
-import sys
-def main(temp_f: float) -> float:
-    return (float(temp_f) - 32) * (5/9)
-
-
 #import sys
-#from scipy.constants import convert_temperature
 #def main(temp_f: float) -> float:
-#    return convert_temperature(float(temp_f), 'F', 'C')
+#    return (float(temp_f) - 32) * (5/9)
+
+
+
+import sys
+from scipy.constants import convert_temperature
+def main(temp_f: float) -> float:
+    return convert_temperature(float(temp_f), 'F', 'C')
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
